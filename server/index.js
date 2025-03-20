c02ccfd639765a91c4705114b08ced4a8f16c2f5
rohan3433 <rohanab343@gmail.com>
2025-03-21 00:13:54 +0530

    Save my changes before pulling

--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js';
+import cors from "cors";
+import userRoutes from './routes/UserRoutes.js';
 
 dotenv.config();
 const app = express();
+app.use(cors());
+
 app.use(express.json()); 
 
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
+app.post("/test", (req, res) => {
+  console.log("Received test request:", req.body);
+  res.json({ message: "Test successful", receivedData: req.body });
+});
+
+
+
 app.use("/api/users", userRoutes);
+
 
 
 
@@ -16,6 +32,12 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 console.log("MONGO_URI:", process.env.MONGO_URI);
+
+console.log(
+  "User routes registered at: http://localhost:" + PORT + "/api/users"
+);
+
+
 
 mongoose
   .connect(MONGO_URI)
